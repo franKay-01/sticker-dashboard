964685d0a76960a465aa3e942cc93d1312fa82e1
pixelbendr <dev@psyphertxt.com>
2016-10-09 00:00:44 +0000

    added cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
  var S3Adapter = require('parse-server').S3Adapter;
 var path = require('path');
-
+var cors = require('cors'); // import the module
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
 if (!databaseUri) {
