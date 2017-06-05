0c67294c20833d980bb25d31b7fc8cb80ed7f1ef
EnigmaDX <derryckdxd@gmail.com>
2017-06-05 16:57:04 +0100

    links to pages

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -3,46 +3,28 @@ $(document).ready(function () {
         {
             dots: true,
             autoplay: true,
-            arrows:false
+            arrows: false
         }
     );
+}
+);
 
     //Create User in Parse
-    Parse.initialize("dash-stickers");
-    Parse.serverURL = 'https://sticker-dashboard.herokuapp.com/parse/';
+//     Parse.initialize("dash-stickers");
+//     Parse.serverURL = 'https://sticker-dashboard.herokuapp.com/parse/';
+//
+//     var user = Parse.User;
+//     user.set("username", "cyfa12");
+//     user.set("password", "hash$1");
+//
+//     $('#loginSubmit').click(function()
+//     {
+//         //check empty fields before proceeding
+//         if($('#uname').val = '')
+//         {
+//
+//         }
+//         user.logIn();
+//     })
+// });
 
-    var user = Parse.User;
-    user.set("username", "cyfa12");
-    user.set("password", "hash$1");
-
-    $('#loginSubmit').click(function()
-    {
-        //check empty fields before proceeding
-        if($('#uname').val = '')
-        {
-
-        }
-        user.logIn();
-    })
-});
-
-//Create User in Parse
-Parse.initialize("dash-stickers");
-Parse.serverURL = 'https://sticker-dashboard.herokuapp.com/parse/';
-
-var user = Parse.User;
-user.set("username", "cyfa12");
-user.set("password", "hash$1");
-
-function signIn (uname, password)
-{
-    user.signUp().then(function(success)
-    {
-        user.logIn(uname, password).then(
-            function success()
-            {
-                window.location.href = "/dashboard";
-            }
-        )
-    });
-}
