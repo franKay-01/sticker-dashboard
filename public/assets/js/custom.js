2d49732dab676ff62cfa817b82ddccf581f8ca6e
EnigmaDX <derryckdxd@gmail.com>
2017-06-06 08:34:51 +0100

    including parse

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,3 +1,7 @@
+//import parse dependencies
+var Parse = require("parse");
+var ParseServer = require('parse-server').ParseServer;
+
 $(document).ready(function () {
     $('.features').slick(
         {
@@ -6,25 +10,22 @@ $(document).ready(function () {
             arrows: false
         }
     );
+
+        // Create User in Parse
+        Parse.initialize("cryptic-waters12");
+        Parse.serverURL = 'https://cryptic-waters-41617.herokuapp.com/parse/';
+
+        var user = Parse.User;
+        user.set("username", "cyfa12");
+        user.set("password", "hash$1");
+
+        // $('#loginSubmit').click(function()
+        // {
+        //
+        // });
+
 }
 );
 
-    //Create User in Parse
-//     Parse.initialize("dash-stickers");
-//     Parse.serverURL = 'https://sticker-dashboard.herokuapp.com/parse/';
-//
-//     var user = Parse.User;
-//     user.set("username", "cyfa12");
-//     user.set("password", "hash$1");
-//
-//     $('#loginSubmit').click(function()
-//     {
-//         //check empty fields before proceeding
-//         if($('#uname').val = '')
-//         {
-//
-//         }
-//         user.logIn();
-//     })
-// });
+
 
