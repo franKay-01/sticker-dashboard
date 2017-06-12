317a944991520029c933b3cf79459cc88d20fc30
EnigmaDX <derryckdxd@gmail.com>
2017-06-12 10:19:01 +0100

    on user's token

--- public/assets/js/validate.js
+++ public/assets/js/validate.js
@@ -2,16 +2,32 @@ Parse.initialize("cryptic-waters12");
 Parse.serverURL = 'https://cryptic-waters-41617.herokuapp.com/parse/';
 
 
-getCookie = function (cname) {
+// getCookie = function (cname) {
+//     var name = cname + "=";
+//     var ca = document.cookie.split(';');
+//     for (var i = 0; i < ca.length; i++) {
+//         var c = ca[i];
+//         while (c.charAt(0) === ' ') c = c.substring(1);
+//         if (c.indexOf(name) !== -1) return c.substring(name.length, c.length);
+//     }
+//     return undefined;
+// };
+
+function getCookie(cname) {
     var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0) === ' ') c = c.substring(1);
-        if (c.indexOf(name) !== -1) return c.substring(name.length, c.length);
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
-    return undefined;
-};
+    return "";
+}
 
 var token = getCookie("token");
 //console.log("token------"+token);
