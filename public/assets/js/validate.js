ca351eb5e0977e0384cfd5a07a519c88f97c8b98
EnigmaDX <derryckdxd@gmail.com>
2017-06-12 11:22:31 +0100

    still working on sessionToken

--- public/assets/js/validate.js
+++ public/assets/js/validate.js
@@ -2,32 +2,16 @@ Parse.initialize("cryptic-waters12");
 Parse.serverURL = 'https://cryptic-waters-41617.herokuapp.com/parse/';
 
 
-// getCookie = function (cname) {
-//     var name = cname + "=";
-//     var ca = document.cookie.split(';');
-//     for (var i = 0; i < ca.length; i++) {
-//         var c = ca[i];
-//         while (c.charAt(0) === ' ') c = c.substring(1);
-//         if (c.indexOf(name) !== -1) return c.substring(name.length, c.length);
-//     }
-//     return undefined;
-// };
-
-function getCookie(cname) {
+getCookie = function (cname) {
     var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
+        while (c.charAt(0) === ' ') c = c.substring(1);
+        if (c.indexOf(name) !== -1) return c.substring(name.length, c.length);
     }
-    return "";
-}
+    return undefined;
+};
 
 var token = getCookie("token");
 //console.log("token------"+token);
@@ -46,7 +30,6 @@ if (token !== "") {
 
             alert(JSON.stringify(error));
                // window.location = "https://cryptic-waters-41617.herokuapp.com/";
-
          });
     }
     else
