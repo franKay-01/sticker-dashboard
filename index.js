9e45fbd96676e41eaac2850e1ae6a5bf1cdd726a
EnigmaDX <derryckdxd@gmail.com>
2017-06-15 12:42:47 +0100

    working on dragndrop feature

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var Parse = require("parse/node"); // import the module
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var cookieSession = require('cookie-session');
+var dropzone = require('dropzone');
 var fs = require('fs');
 //var multer  = require('multer');
 
