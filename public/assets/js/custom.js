a99b051a1a3573992295ca3f786420faec6fb9a4
EnigmaDX <derryckdxd@gmail.com>
2017-06-15 09:08:35 +0100

    working on upload img

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
             var reader = new FileReader();
 
             reader.onload = function (e) {
-                $('#left-col').attr('src', e.target.result);
+                $('#add-img').attr('src', e.target.result);
             }
             reader.readAsDataURL(input.files[0]);
         }
