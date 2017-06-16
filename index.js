b8f3c2ea59f5ada4c3cf96d0c60a819694b1b6b3
EnigmaDX <derryckdxd@gmail.com>
2017-06-16 13:16:29 +0100

    try to print file details

--- index.js
+++ index.js
@@ -169,7 +169,6 @@ app.post('/upload', function (req, res)
     var session = req.session.token;
     var token = req.cookies.token;
 
-    console.log("FILE INFO " + JSON.stringify(req.files.tfile));
     console.log("FILE INFO " + JSON.stringify(req.files.ffile));
     console.log("BODY INFO " + JSON.stringify(req.body));
 
