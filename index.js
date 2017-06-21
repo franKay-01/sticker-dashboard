3a4abb0914be92424249efe204e6c031f6872c88
EnigmaDX <derryckdxd@gmail.com>
2017-06-21 10:15:12 +0100

    checking if file has saved

--- index.js
+++ index.js
@@ -227,7 +227,7 @@ app.post('/uploads', upload.single('ffile'), function (req, res)
 
         var StickerObject = new Parse.Object.extend("Stickers");
         var parseFile = new Parse.File(stickerName, ndata);
-        console.log("Parse File:::::::" + JSON.stringify(parseFile));
+        console.log("Parse File::::::::::" + JSON.stringify(parseFile));
 
         parseFile.save().then(function()
         {
