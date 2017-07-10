998026d578c36e3213a0bf45600330c270c50943
EnigmaDX <derryckdxd@gmail.com>
2017-07-10 15:05:29 +0100

    trying new query for coll's stickers

--- index.js
+++ index.js
@@ -441,12 +441,9 @@ app.get('/collection/:id', function (req, res) {
     if (session && token) {
 
         //TODO get current collection's id and find all stickers that are linked to it
-        var coll = Parse.Object.extend("Collection");
-        var collq = coll.relation("Sticker");
-
         var sticker = new Parse.Query("Stickers");
-        // sticker.equalTo("parent", coll_id);
-        collq.query().find({sessionToken: token}).then(function (stickers) {
+        sticker.include("arrays");
+        sticker.find({sessionToken: token}).then(function (stickers) {
 
                 if (stickers) {
 
