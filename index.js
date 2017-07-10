9d18bb3cbad678948e80f04b8e49742caa1dcc90
EnigmaDX <derryckdxd@gmail.com>
2017-07-10 11:33:03 +0100

    testing parent relation with sticker

--- index.js
+++ index.js
@@ -304,10 +304,12 @@ app.post('/upload', upload.array('im1[]'), function (req, res) {
                 //GET ID OF CURRENT COLLECTION
                 var colq = new Parse.Query("Collection");
                 colq.equalTo("collection_name", "Ghamoji");
+                console.log("Searching for collection.........");
                 colq.first({sessionToken: token}).then(function (collection){
                         console.log("Current Collection====== " + JSON.stringify(collection));
                         var collection_relation = collection.relation("Collection");
                         collection_relation.add(sticker);
+                        sticker.set("parent", collection);
                         console.log("Relation added to collection class");
                         collection.save();
                     },
