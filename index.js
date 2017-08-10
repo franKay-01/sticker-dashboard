2ced10d94f1b1d8d4df4d20b2acd798dda9650d4
EnigmaDX <derryckdxd@gmail.com>
2017-08-10 15:43:05 +0100

    testing new promises

--- index.js
+++ index.js
@@ -561,6 +561,26 @@ app.get('/collection/:id', function (req, res) {
     var token = req.cookies.token;
     var coll_id = req.params.id;
 
+
+    var query = new Parse.Query("Comments");
+    query.equalTo("post", post);
+
+    query.find().then(function(results) {
+        // Create a trivial resolved promise as a base case.
+        var promise = Parse.Promise.as();
+        _.each(results, function(result) {
+            // For each item, extend the promise with a function to delete it.
+            promise = promise.then(function() {
+                // Return a promise that will be resolved when the delete is finished.
+                return result.destroy();
+            });
+        });
+        return promise;
+
+    }).then(function() {
+        // Every comment was deleted.
+    });
+
     if (session && token) {
 
         var collection = new Parse.Query("Collection");
