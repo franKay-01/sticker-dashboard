cdb85f9264b38bc809bd467e4acfed3aa9fb5d36
pixelbendr <dev@psyphertxt.com>
2017-08-11 15:26:54 +0000

    checking body output

--- index.js
+++ index.js
@@ -742,6 +742,9 @@ app.post('/update/:id', upload.single('im1'), function (req, res) {
             new Parse.Query("Category").containedIn("objectId",category)
         ).then(function(sticker,categories){
 
+                console.log("STICKER " + JSON.stringify(sticker));
+                console.log("CATEGORIES " + JSON.stringify(categories));
+
             if(sticker && categories.length){
                 console.log("STICKER " + JSON.stringify(sticker));
                 console.log("CATEGORIES " + JSON.stringify(categories));
