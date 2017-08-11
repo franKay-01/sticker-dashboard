3d6af94f7110db1be99b059f4031a32150f01460
EnigmaDX <derryckdxd@gmail.com>
2017-08-11 16:55:07 +0100

    redo route after update

--- index.js
+++ index.js
@@ -579,33 +579,33 @@ app.get('/collection/:id', function (req, res) {
 
                      if (stickers.length) {
 
-                         _stickers = stickers;
+                     _stickers = stickers;
 
-                         _.each(stickers, function (sticker) {
+                     _.each(stickers, function (sticker) {
 
 
-                             var query = sticker.relation("cat");
-                             return query.find();
+                     var query = sticker.relation("cat");
+                     return query.find();
 
-                             promise = promise.then(function () {
+                     promise = promise.then(function () {
 
-                                 console.log("STICKER " + JSON.stringify(sticker));
+                     console.log("STICKER " + JSON.stringify(sticker));
 
-                                 var query = sticker.relation("cat");
-                                 return query.find();
-                             });
-                         });
+                     var query = sticker.relation("cat");
+                     return query.find();
+                     });
+                     });
 
                      }
 
-                    return promise;*/
+                     return promise;*/
 
                     /*
-                    * .then(function (categories) {
+                     * .then(function (categories) {
 
                      console.log("CATEGORIES " + JSON.stringify(categories));
 
-                        var _categoryName = [];
+                     var _categoryName = [];
                      _.each(categories, function (category) {
                      _categoryName.push(category.get("name"))
                      });
@@ -730,14 +730,12 @@ app.post('/update/:id', upload.single('im1'), function (req, res) {
     var imgChange = req.body.imgChange;
     var stickerId = req.params.id;
 
-console.log("STICKER ID::::::::::" + JSON.stringify(stickerId));
+    console.log("STICKER ID::::::::::" + JSON.stringify(stickerId));
     if (session && token) {
 
         Parse.Promise.when(
-
             new Parse.Query("Sticker").equalTo("objectId", stickerId).first(),
             new Parse.Query("Category").containedIn("objectId", category).find()
-
         ).then(function (sticker, categories) {
             console.log("STICKER ::::::::::" + JSON.stringify(sticker));
 
@@ -775,8 +773,8 @@ console.log("STICKER ID::::::::::" + JSON.stringify(stickerId));
                     console.log("Could not del temp++++++++" + JSON.stringify(err));
                 }
             });
-
-            res.redirect("/collections-dashboard");
+            console.log("FILE UPDATED SUCCESSFULLYYYY");
+            res.redirect("/dashboard");
 
         }, function (e) {
             console.log("SERVER ERROR " + e.message);
@@ -793,97 +791,97 @@ console.log("STICKER ID::::::::::" + JSON.stringify(stickerId));
 });
 
 /*
-app.post('/update/:id', upload.single('im1'), function (req, res) {
+ app.post('/update/:id', upload.single('im1'), function (req, res) {
 
-    var session = req.session.token;
-    var token = req.cookies.token;
+ var session = req.session.token;
+ var token = req.cookies.token;
 
-    //input fields from form
-    var stickerName = req.body.stickername;
-    var category = req.body.cat;
-    var file = req.file;
-    var imgChange = req.body.imgChange;
-    var stickerId = req.params.id;
+ //input fields from form
+ var stickerName = req.body.stickername;
+ var category = req.body.cat;
+ var file = req.file;
+ var imgChange = req.body.imgChange;
+ var stickerId = req.params.id;
 
-    console.log("BODY-------------------" + JSON.stringify(req.body));
+ console.log("BODY-------------------" + JSON.stringify(req.body));
 
-    if (session && token) {
+ if (session && token) {
 
-        var categoryQuery = new Parse.Query("Category");
+ var categoryQuery = new Parse.Query("Category");
 
-        var categoryArray = category.split(", ");
-        //query for existing categories in parse
-        categoryArray.forEach(function (category, index) {
-            console.log("Item " + [index] + "::: " + category);
+ var categoryArray = category.split(", ");
+ //query for existing categories in parse
+ categoryArray.forEach(function (category, index) {
+ console.log("Item " + [index] + "::: " + category);
 
-            categoryQuery.equalTo("name", category);
-            categoryQuery.find().then(function (catgory) {
-                    console.log("Category*****************" + JSON.stringify(catgory));
+ categoryQuery.equalTo("name", category);
+ categoryQuery.find().then(function (catgory) {
+ console.log("Category*****************" + JSON.stringify(catgory));
 
-                    var NewSticker = new Parse.Object.extend("Sticker");
-                    var sticker = new Parse.Query(NewSticker);
-                    sticker.equalTo("objectId", stickerId);
-                    sticker.first({sessionToken: token}).then(
-                        function (newSticker) {
-                            //Update new sticker properties
-                            newSticker.set("stickerName", stickerName);
-                            newSticker.set("localName", stickerName);
-                            newSticker.add("category", catgory);
+ var NewSticker = new Parse.Object.extend("Sticker");
+ var sticker = new Parse.Query(NewSticker);
+ sticker.equalTo("objectId", stickerId);
+ sticker.first({sessionToken: token}).then(
+ function (newSticker) {
+ //Update new sticker properties
+ newSticker.set("stickerName", stickerName);
+ newSticker.set("localName", stickerName);
+ newSticker.add("category", catgory);
 
-                            if (imgChange === 'true') {
-                                //update sticker image
-                                var bitmap = fs.readFileSync(file.path, {encoding: 'base64'});
-                                var parseFile = new Parse.File(stickerName, {base64: bitmap}, file.mimetype);
-                                newSticker.set("uri", parseFile);
-                            }
-                            else {
-                                //image has not changed
-                            }
-                            //Update sticker's properties to parse
-                            newSticker.save().then(function () {
-                                    var sticker_relation = catgory.relation("sticker");
-                                    sticker_relation.add(newSticker);
-                                    catgory.save();
+ if (imgChange === 'true') {
+ //update sticker image
+ var bitmap = fs.readFileSync(file.path, {encoding: 'base64'});
+ var parseFile = new Parse.File(stickerName, {base64: bitmap}, file.mimetype);
+ newSticker.set("uri", parseFile);
+ }
+ else {
+ //image has not changed
+ }
+ //Update sticker's properties to parse
+ newSticker.save().then(function () {
+ var sticker_relation = catgory.relation("sticker");
+ sticker_relation.add(newSticker);
+ catgory.save();
 
-                                    //Delete tmp fil after update
-                                    var tempFile = file.path;
-                                    fs.unlink(tempFile, function (err) {
-                                        if (err) {
-                                            //TODO handle error code
-                                            console.log("Could not del temp++++++++" + JSON.stringify(err));
-                                        }
-                                    });
-                                },
-                                function (problem) {
-                                    //sticker not updated...reload page
-                                    //TODO handle error code
-                                    console.error("Update unsuccessful__ " + JSON.stringify(problem));
-                                    res.redirect("/details", {id: stickerId});
-                                }
-                            );
-                        },
-                        function (error) {
-                            //TODO handle error code
-                            console.log("STICKER NOT FOUND: " + JSON.stringify(error))
-                        }
-                    );
-                },
-                function (error) {
-                    console.error("Error" + error);
-                });
-        });
+ //Delete tmp fil after update
+ var tempFile = file.path;
+ fs.unlink(tempFile, function (err) {
+ if (err) {
+ //TODO handle error code
+ console.log("Could not del temp++++++++" + JSON.stringify(err));
+ }
+ });
+ },
+ function (problem) {
+ //sticker not updated...reload page
+ //TODO handle error code
+ console.error("Update unsuccessful__ " + JSON.stringify(problem));
+ res.redirect("/details", {id: stickerId});
+ }
+ );
+ },
+ function (error) {
+ //TODO handle error code
+ console.log("STICKER NOT FOUND: " + JSON.stringify(error))
+ }
+ );
+ },
+ function (error) {
+ console.error("Error" + error);
+ });
+ });
 
-        res.redirect("/dashboard");
-    }
-    else {
-        function problem(error) {
-            //TODO handle error code
-            console.log("No session found[[[[[[" + error);
-            res.redirect("/details");
-        }
-    }
-});
-* */
+ res.redirect("/dashboard");
+ }
+ else {
+ function problem(error) {
+ //TODO handle error code
+ console.log("No session found[[[[[[" + error);
+ res.redirect("/details");
+ }
+ }
+ });
+ * */
 
 
 var port = process.env.PORT || 1337;
