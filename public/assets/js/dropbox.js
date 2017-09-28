8180f9881855f979e5b7f35befb3f34b1e788bf8
pixelbendr <dev@psyphertxt.com>
2017-09-28 17:40:30 +0000

    added dropbox.js

---
+++ public/assets/js/dropbox.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+
+    //dropbox choose options
+    options = {
+        success: function (file) {
+            // files.forEach(function(file) {
+            //     dropboxImageSelected(file);
+            // });
+            dropboxImageSelected(file);
+        },
+        cancel: function () {
+            //optional
+        },
+        linkType: "direct", // "preview" or "direct"
+        multiselect: false, // true or false
+        //  extensions: ['.png', '.jpg'] //restrict to curtain extensions
+        extensions: ['images'] //restrict to curtain extensions
+    };
+
+    //show dropbox button in view
+    //TODO learn how to customize
+    var button = Dropbox.createChooseButton(options);
+    $("#dropbox-container").append(button);
+
+    function dropboxImageSelected(file) {
+
+        var reader = new FileReader();
+
+        reader.onloadend = function () {
+            var span = $('<span></span>');
+            span.html(['<img class="thumb" name="im1" " src="', reader.result,
+                '" title="', escape(file.name), '"/>'].join(''));
+
+            $('#filesList').before(span);
+        };
+        reader.readAsDataURL(file);
+
+    }
+
+    var imageToBase64 = function (url, callback) {
+        var img = new Image();
+
+        img.crossOrigin = 'Anonymous';
+
+        img.onload = function () {
+            var canvas = document.createElement('CANVAS');
+            var ctx = canvas.getContext('2d');
+            var dataURL;
+            canvas.height = this.height;
+            canvas.width = this.width;
+            ctx.drawImage(this, 0, 0);
+            dataURL = canvas.toDataURL();
+            callback(dataURL);
+            canvas = null;
+        };
+
+        img.src = url;
+    }
+
+
+});
