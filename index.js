4270b5c92d8d5d4547097bbddf929a2008fc611c
franciskornu-09 <franciskornu@gmail.com>
2017-10-11 13:53:40 +0000

    trying new upload with FS

--- index.js
+++ index.js
@@ -837,12 +837,12 @@ app.post('/upload-file', function (req, res) {
 
         
         // Convert url link to base64 encoded data
-         var newPath = __dirname + "/uploads" + name;
+         var newPath = __dirname + "/uploads/" + name;
          fs.writeFile(newPath, bitmap, function(err){
             if (err) {
                 console.log("NOT NOT "+err);
             }else{
-                console.log("CONGERATS");
+                res.redirect("/uploads/" + name);
             }
          });
         // i2b(fileUrl, function (err, data) {
