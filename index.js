f3220132666027007a985228399fbf0f0ad24eeb
franciskornu-09 <franciskornu@gmail.com>
2017-10-26 14:32:23 +0000

    displaying current user

--- index.js
+++ index.js
@@ -195,7 +195,7 @@ app.get('/home', function(req, res){
     var session = req.session.token;
     var token = req.cookies.token;
     var user = req.user;
-    console.log("NAME OF USER "+JSON.stringify(user.get("name"))); 
+    console.log("NAME OF USER "+JSON.stringify(user)); 
     var pack = [];
     var pack_category = [];
 
