ffa527612fd6ff03832606f3c4ab6dd97057c738
Francis Kornu <franciskornu@Franciss-MacBook-Pro.local>
2017-11-17 10:16:04 +0000

    handling errors

--- index.js
+++ index.js
@@ -357,10 +357,14 @@ app.post('/find_category', function (req, res) {
                 // var name = category.get("name");
                 // var _id = category.get("id");
             console.log("MESSAGE FROM SEARCH "+ category);
-
+                if (category !== undefined) {
                     console.log("CATEGORY DETAILS " + JSON.stringify(category));
                     res.render("pages/search_categories", {category_details: category});
-
+                }else {
+                    console.log("No categories found.............." + JSON.stringify(error));
+                    searchErrorMessage = error.message;
+                    res.redirect("/categories");
+                }
             },
             function (error) {
                 console.log("No categories found.............." + JSON.stringify(error));
