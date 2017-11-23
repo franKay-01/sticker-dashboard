513cadb1ba98a6f844505ce7f7ae796cda8cb6be
pixelbendr <dev@psyphertxt.com>
2017-11-23 16:13:31 +0000

    minor changes to get stickers

--- cloud/functions/getStickers.js
+++ cloud/functions/getStickers.js
@@ -5,13 +5,26 @@ var stickers = require("../modules/stickers");
 
 Parse.Cloud.define("getStickers", function (req, res) {
 
-   // var user = req.user;
+    // var user = req.user;
 
-    stickers.getAll().then(function(stickers){
+    stickers.getAll().then(function (stickers) {
+
+        var sticker_relation = stickers.relation("Category");
+        return sticker_relation.query().find();
+
+    }).then(function (stickerCategories) {
+
+        var categoryNames = [];
+        if (stickerCategories.length) {
+            _.each(stickerCategories, function (category) {
+                categoryNames.push(category.get("name"))
+            });
+        }
 
         res.success(util.setResponseOk(stickers));
 
-    },function(error){
+
+    }, function (error) {
 
         util.handleError(res, error);
 
@@ -22,7 +35,7 @@ Parse.Cloud.define("getStickers", function (req, res) {
 Parse.Cloud.define("getSticker", function (req, res) {
 
     var stickerId = req.params.stickerId;
-   // var user = req.user;
+    // var user = req.user;
 
     stickers.findById(stickerId).then(function (badge) {
 
