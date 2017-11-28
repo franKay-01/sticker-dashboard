9221b3471979f6e4c2b19098552869fb86312f38
Francis Kornu <franciskornu@Franciss-MacBook-Pro.local>
2017-11-28 11:07:09 +0000

    trying to upload multiple stickers from dropbox

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
 
         //clear form
         $('#clearBtn').on('click', function () {
-            document.getElementById("pack_id").reset();
+            $("#filesList").remove();
         });
 
         //close/hide form
