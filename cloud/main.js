7a428ea9f080384ca3f9fc7550ce8aaed842d3dc
Francis Kornu <franciskornu@Franciss-MacBook-Pro.local>
2017-12-05 11:44:03 +0000

    testing cloud functions

--- cloud/main.js
+++ cloud/main.js
@@ -1,7 +1,7 @@
 //used to retrieve all stickers
 let Parse = require("parse/node").Parse;
 
-require("./functions/getStickers");
+//require("./functions/getStickers");
 
 Parse.Cloud.job("sticker_number", function(request, status) {
      new Parse.Query("Stickers").count().then(function (result) {
