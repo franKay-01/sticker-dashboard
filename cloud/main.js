c8edff31c5b0f29b58ad961213672b49e2e8d0c3
pixelbendr <dev@psyphertxt.com>
2017-12-05 18:00:26 +0000

    jobs

--- cloud/main.js
+++ cloud/main.js
@@ -1,7 +1,7 @@
 //used to retrieve all stickers
 //require("./functions/getStickers");
 
-Parse.Cloud.job("stickerNumber", function(request, status) {
+Parse.Cloud.define("stickerNumber", function(request, status) {
      new Parse.Query("Stickers").count().then(function (result) {
         console.log("GOT THIS "+ JSON.stringify(result));
      });
