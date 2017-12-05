6d35893d88f975247b31ab597e5efebaf2f72b0c
Francis Kornu <franciskornu@Franciss-MacBook-Pro.local>
2017-12-05 10:27:01 +0000

    defining jobs

--- cloud/main.js
+++ cloud/main.js
@@ -1,2 +1,8 @@
 //used to retrieve all stickers
 require("./functions/getStickers");
+
+Parse.Cloud.job("sticker_number", function(request, status) {
+     new Parse.Query(Stickers).count().then(function (result) {
+        console.log("GOT THIS "+ JSON.stringify(result));
+     });
+});
