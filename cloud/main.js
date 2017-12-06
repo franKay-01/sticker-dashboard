4b7fd1d22fd70e5c96192fcb2caa7d8f14d2b55a
Francis Kornu <franciskornu@Franciss-MacBook-Pro.local>
2017-12-06 13:12:51 +0000

    hscking stats save

--- cloud/main.js
+++ cloud/main.js
@@ -1,24 +1,34 @@
 //used to retrieve all stickers
 //require("./functions/getStickers");
-let StickerCLass = "Stickers";
-let CategoryClass ="Categories";
+let StickerClass = "Stickers";
+let CategoryClass = "Categories";
 let StatsClass = "Stats";
+let PacksClass = "Packs";
 
-Parse.Cloud.define("stickerNumber", function(request, status) {
+Parse.Cloud.define("stickerNumber", function (req, res) {
+
+    /*STICKERS=0
+    PACKS=1
+    CATEGORIES=2*/
 
     Parse.Promise.when(
-        new Parse.Query(StickerCLass).count(),
-        new Parse.Query(CategoryClass).count()).then(function (stickers, category) {
+        new Parse.Query(StickerClass).count(),
+        new Parse.Query(PacksClass).count(),
+        new Parse.Query(CategoryClass).count()).then(function (stickers, packs, categories) {
 
-        var Stats = new Parse.Object.extend(StatsClass);
-        var stats = new Stats();
-            stats.set("stickers", stickers);
-            stats.set("categories", category);
-            return stats.save();
-        }).then(function () {
-            console.log("STATS UPDATED");
-        }), function (error) {
-            console.log("ERROR OCCURRED " + error.message);
-        }
+        //stickers .equalTo("objectId","R0ux0VzLB2")
+        //packs .equalTo("objectId","pjTizUehrT")
+        //categories equalTo("objectId","2NKxat6SPF")
+
+        return Parse.Promise.when(
+            new Parse.Query(StatsClass).equalTo("objectId", "R0ux0VzLB2").set("count", stickers).save(),
+            new Parse.Query(PacksClass).equalTo("objectId", "pjTizUehrT").set("count", packs).save(),
+            new Parse.Query(CategoryClass).equalTo("objectId", "2NKxat6SPF").set("count", categories).save());
+
+    }).then(function (stickers, packs, categories) {
+        req.success();
+    }, function (error) {
+        console.log("ERROR OCCURRED " + error.message);
+    });
 
 });
