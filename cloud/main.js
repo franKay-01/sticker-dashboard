f22a3412866e3b6a066b853a0bd4122ecc8a6a4f
Francis Kornu <franciskornu@Franciss-MacBook-Pro.local>
2017-12-06 11:05:28 +0000

    testing user details

--- cloud/main.js
+++ cloud/main.js
@@ -2,7 +2,10 @@
 //require("./functions/getStickers");
 
 Parse.Cloud.define("stickerNumber", function(request, status) {
+    var user = request.params.username;
+
      new Parse.Query("Stickers").count().then(function (result) {
         console.log("GOT THIS "+ JSON.stringify(result));
+        console.log("USER DETAILS " + JSON.stringify(user));
      });
 });
