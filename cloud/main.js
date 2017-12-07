3c95e85bb3b9ad4bbd2d87e57570179add064eec
Francis Kornu <franciskornu@Franciss-MacBook-Pro.local>
2017-12-07 16:45:55 +0000

    removed unwanted code

--- cloud/main.js
+++ cloud/main.js
@@ -23,26 +23,19 @@ Parse.Cloud.define("stickerNumber", function (req, res) {
             _packs =  packs;
             _categories = categories;
 
-          //  if (stickers.length && packs.length && categories.length) {
                 return Parse.Promise.when(
                     new Parse.Query(StatsClass).equalTo("objectId", "R0ux0VzLB2").first(),
                     new Parse.Query(StatsClass).equalTo("objectId", "pjTizUehrT").first(),
                     new Parse.Query(StatsClass).equalTo("objectId", "2NKxat6SPF").first());
-          //  }
 
-          //  req.error()
 
         }).then(function (sticker, pack, category) {
 
-    //    if (sticker && pack && category) {
             return Parse.Promise.when(
                 sticker.set("count", _stickers).save(),
                 pack.set("count", _packs).save(),
                 category.set("count", _categories).save());
 
-     //   }
-
-       // req.error();
 
     }).then(function () {
         req.success();
