6f71530e085f68b62a66cec8445eebeab5ca303c
Francis Kornu <franciskornu@Franciss-MacBook-Pro.local>
2017-12-07 16:04:37 +0000

    working on token

--- public/assets/js/validate.js
+++ public/assets/js/validate.js
@@ -21,6 +21,7 @@ if (token !== "") {
     if (Parse.User.current() === null) {
 
         token = "r:"+token.substring(2);
+        console.log("TOKEN FROM USER "+JSON.stringify(token));
         Parse.User.become(token,{
             success:function(){
                 console.log("Current user::::::" + Parse.User.current());
