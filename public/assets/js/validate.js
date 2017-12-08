78f962f9f054890a99665388c34edbbc5b0b323b
Francis Kornu <franciskornu@Franciss-MacBook-Pro.local>
2017-12-08 14:11:40 +0000

    trying to get token in validate

--- public/assets/js/validate.js
+++ public/assets/js/validate.js
@@ -19,7 +19,7 @@ var token = getCookie("token");
 if (token !== "") {
 
     if (Parse.User.current() === null) {
-
+        console.log("FIRST TOKEN "+JSON.stringify(token));
         token = "r:"+token.substring(2);
         console.log("TOKEN FROM USER "+JSON.stringify(token));
         // Parse.User.become(token,{
