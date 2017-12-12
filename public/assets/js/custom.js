1fa8cd276a0103e1005262b754c78c09fdaaf741
Francis Kornu <franciskornu@Franciss-MacBook-Pro.local>
2017-12-12 17:38:13 +0000

    work on delete sticker

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -183,6 +183,7 @@ $(document).ready(function () {
         $('#Welcome').fadeOut('fast');
         $('#signoutLink').fadeOut('fast');
         $('#opaqueDiv').fadeOut('fast');
+        $('#detailsForm').fadeOut('fast');
 
         //insert category name into text field for removal
         // $('#catNameRemove').val($this.val());
