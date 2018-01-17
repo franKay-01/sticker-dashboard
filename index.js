eb5630f4ed7d20e2848bfbb25b8536a8da28c2ab
Francis Kornu <franciskornu@Franciss-MacBook-Pro.local>
2018-01-17 14:22:29 +0000

    verifying emails

--- index.js
+++ index.js
@@ -351,6 +351,8 @@ app.get('/set_password', function (req, res) {
 });
 
 app.get('/verification', function (req, res) {
+    var session = req.session.token;
+    console.log("VERIFICATION TOKEN FROM BROWSER"+session);
     // var query = new Parse.Query(Parse.User);
     // query.equalTo("email", "fkay0450@gmail.com");
     //
