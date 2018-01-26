d62a440b709948cfd4c2b953ca242b591b9caed0
Francis Kornu <franciskornu@Franciss-MacBook-Pro.local>
2018-01-26 09:47:15 +0000

    trying to pull using user

--- index.js
+++ index.js
@@ -641,7 +641,8 @@ app.get('/user_profile', function (req, res) {
 
     if (session && token) {
         new Parse.Query("User").equalTo("objectId", user_info).find({sessionToken: token}).then(function (user) {
-
+            var _user = user.get("image");
+            console.log(JSON.stringify(_user));
             // var _image = JSON.stringify(_profile);
             // console.log("USER PROFILE " + _image.url());
             // if(user.get("image").url() !== undefined){
