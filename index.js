4e72ba9c20739ea1590a32e7e05546a2438a7bbf
Francis Kornu <franciskornu@Franciss-MacBook-Pro.local>
2018-02-01 12:52:50 +0000

    review page

--- index.js
+++ index.js
@@ -633,7 +633,7 @@ app.get('/review/:id', function (req, res) {
                 var pack_owner = pack.get("user_id");
                 var _owner = [];
 
-                console.log("ABOUT TO SEARCH FOR USER");
+                console.log("ABOUT TO SEARCH FOR USER "+pack_owner);
                 new Parse.Query("User").equalTo("objectId", pack_owner).get().then(function (user) {
                     _owner = user;
                     console.log("USER TO BE REVIEWED "+JSON.stringify(_owner));
