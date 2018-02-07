2a5b180040bd1056d9f87a64adb81419733ec69f
Francis Kornu <franciskornu@Franciss-MacBook-Pro.local>
2018-02-07 10:13:07 +0000

    session code

--- index.js
+++ index.js
@@ -172,6 +172,7 @@ app.use(function (req, res, next) {
         randomNumber=randomNumber.substring(2,randomNumber.length);
         res.cookie('cookieName',randomNumber, { maxAge: 900000, httpOnly: true });
         console.log('cookie created successfully');
+        console.log("COOKIE "+randomNumber);
     }
     else
     {
