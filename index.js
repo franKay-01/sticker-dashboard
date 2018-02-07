1541742a4148b30d9e12bad5df8c40c7e8098a6a
Francis Kornu <franciskornu@Franciss-MacBook-Pro.local>
2018-02-07 10:48:11 +0000

    session code

--- index.js
+++ index.js
@@ -322,7 +322,7 @@ app.post('/login', function (req, res) {
         res.cookie('profile_image', user.get("image").url());
 
 
-        req.session.token = user.getSessionToken();
+        req.session.token = res.cookie.token;
 
         new Parse.Query('_Session')
             .equalTo('sessionToken', user.getSessionToken())
