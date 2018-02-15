bf82170baec9482c38a759f2364e8c30d571fe91
Francis Kornu <franciskornu@Franciss-MacBook-Pro.local>
2018-02-15 11:38:16 +0000

    commented out logout.js

--- public/assets/js/logout.js
+++ public/assets/js/logout.js
@@ -1,10 +1,10 @@
 /**
  * Created by derry on 22-Jun-17.
  */
-$(document).ready(function () {
-
-    $('#signoutLink').onclick(function () {
-        window.location = '/logout';
-    });
-
-});
+// $(document).ready(function () {
+//
+//     $('#signoutLink').onclick(function () {
+//         window.location = '/logout';
+//     });
+//
+// });
