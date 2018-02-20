0716d4a913fff067067a30cb7a91597e060b9ff6
pixelbendr <dev@psyphertxt.com>
2018-02-20 14:18:39 +0000

    added function to query user

--- index.js
+++ index.js
@@ -433,7 +433,7 @@ app.get('/home', function (req, res) {
 
     if (token) {
 
-        new Parse.Query('_User')
+        new Parse.Query('User')
             .equalTo('sessionToken', token)
             .include('user').first().then(function (user) {
             console.log("SESSION DATA: " + JSON.stringify(user));
