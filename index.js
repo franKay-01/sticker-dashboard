e7ad59d9ff99c7ff6be5b3bbdd85676638a79147
pixelbendr <dev@psyphertxt.com>
2018-02-28 12:00:58 +0000

    disabled ACL

--- index.js
+++ index.js
@@ -600,7 +600,7 @@ app.post('/uploads', upload.array('im1[]'), function (req, res) {
                     sticker.set("flag", false);
                     sticker.set("archive", false);
                     sticker.set("sold", true);
-                    sticker.setACL(setPermission(_user, false));
+                   // sticker.setACL(setPermission(_user, false));
 
                     stickerDetails.push(sticker);
                     fileDetails.push(file);
@@ -1209,7 +1209,7 @@ app.post('/new_pack', upload.array('art'), function (req, res) {
             pack.set("archive", false);
             pack.set("keyword", _keywords);
             pack.set("flag", false);
-            pack.setACL(setPermission(_user, false));
+          //  pack.setACL(setPermission(_user, false));
 
 
             if (files.length !== 0) {
