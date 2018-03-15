b91cd188d4ced9d6953e0b4e8e42ed87dde13c19
Francis Kornu <franciskornu@Francis-MacBook-Pro.local>
2018-03-15 10:54:45 +0000

    added limit on find category and stories

--- index.js
+++ index.js
@@ -558,8 +558,9 @@ app.get('/admin_home', function (req, res) {
                 _story = story;
 
                 _.each(_story, function (stories) {
-                    _story["title"] =  helper.text_truncate(stories.get("title"),7);
-                    console.log("TRANCATED "+helper.text_truncate(stories.get("title"),7));
+                    console.log("TRANCATED "+ stories["title"]);
+                    stories["title"] =  helper.text_truncate(stories.get("title"),15);
+                    console.log("TRANCATED "+ stories["title"]);
                 });
 
             }
