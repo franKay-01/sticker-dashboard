130a2b8dae070f21c0d2c80d85149d7e371e14c3
franciskornu-09 <franciskornu@gmail.com>
2018-03-27 12:53:39 +0000

    working on validating password

--- public/assets/js/functions.js
+++ public/assets/js/functions.js
@@ -13,42 +13,42 @@
             return false;
         }
 
-        if(form.pwd1.value !== "" && form.pwd1.value === form.pwd2.value) {
-            if(form.pwd1.value.length < 6) {
+        if(form.password.value !== "" && form.password.value === form.confirm_password.value) {
+            if(form.password.value.length < 6) {
                 alert("Error: Password must contain at least six characters!");
-                form.pwd1.focus();
+                form.password.focus();
                 return false;
             }
-            if(form.pwd1.value === form.username.value) {
+            if(form.password.value === form.username.value) {
                 alert("Error: Password must be different from Username!");
-                form.pwd1.focus();
+                form.password.focus();
                 return false;
             }
             re = /[0-9]/;
-            if(!re.test(form.pwd1.value)) {
+            if(!re.test(form.password.value)) {
                 alert("Error: password must contain at least one number (0-9)!");
-                form.pwd1.focus();
+                form.password.focus();
                 return false;
             }
             re = /[a-z]/;
-            if(!re.test(form.pwd1.value)) {
+            if(!re.test(form.password.value)) {
                 alert("Error: password must contain at least one lowercase letter (a-z)!");
-                form.pwd1.focus();
+                form.password.focus();
                 return false;
             }
             re = /[A-Z]/;
-            if(!re.test(form.pwd1.value)) {
+            if(!re.test(form.password.value)) {
                 alert("Error: password must contain at least one uppercase letter (A-Z)!");
-                form.pwd1.focus();
+                form.password.focus();
                 return false;
             }
         } else {
             alert("Error: Please check that you've entered and confirmed your password!");
-            form.pwd1.focus();
+            form.password.focus();
             return false;
         }
 
-        alert("You entered a valid password: " + form.pwd1.value);
+        alert("You entered a valid password: " + form.password.value);
         return true;
     }
 
