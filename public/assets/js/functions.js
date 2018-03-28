11dc6f5d223c629a730aa6cd41765ef722a50ca5
franciskornu-09 <franciskornu@gmail.com>
2018-03-28 10:36:30 +0000

    working on validating email on forgot password

--- public/assets/js/functions.js
+++ public/assets/js/functions.js
@@ -1,162 +1,100 @@
-
-    function checkForm(form)
-    {
-        if(form.name.value === "") {
-            document.getElementById("nameField").innerHTML = "Username cannot be blank";
-            form.name.focus();
-            return false;
-        }
-
-        function validateEmail(email) {
-            var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            return re.test(email);
-        }
-        var truth = validateEmail(form.username.value);
-
-        if (truth !== true){
-            document.getElementById("user").innerHTML = "Email Format is not correct";
-            form.username.focus();
-            return false;
-        }else {
-            document.getElementById("user").innerHTML = "";
-
-        }
-
-        if (document.getElementById('terms_policy').checked !== true){
-            document.getElementById("policy").innerHTML = "Please Read the policy and tick ✔";
-            return false;
-        }else {
-            document.getElementById("policy").innerHTML = "";
-
-        }
-
-        if (form.password.value === form.confirm_password.value){
-            document.getElementById("confirm").innerHTML = "";
-        }
-
-        if(form.password.value !== "" && form.password.value === form.confirm_password.value) {
-            if(form.password.value.length < 8) {
-                document.getElementById("pwd").innerHTML = "Password must contain at least eight (8) characters";
-                form.password.focus();
-                return false;
-            }else {
-                document.getElementById("pwd").innerHTML = "";
-            }
-
-            if(form.password.value === form.username.value) {
-                document.getElementById("pwd").innerHTML = "Password must be different from Email";
-                form.password.focus();
-                return false;
-            }else {
-                document.getElementById("pwd").innerHTML = "";
-            }
-
-            re = /[0-9]/;
-            if(!re.test(form.password.value)) {
-                document.getElementById("pwd").innerHTML = "Password must contain at least one number (0-9)";
-                form.password.focus();
-                return false;
-            }else {
-                document.getElementById("pwd").innerHTML = "";
-            }
-
-            re = /[a-z]/;
-            if(!re.test(form.password.value)) {
-                document.getElementById("pwd").innerHTML = "Password must contain at least one lowercase letter (a-z)!";
-                form.password.focus();
-                return false;
-            }else {
-                document.getElementById("pwd").innerHTML = "";
-            }
-
-            re = /[A-Z]/;
-            if(!re.test(form.password.value)) {
-                document.getElementById("pwd").innerHTML = "Password must contain at least one uppercase letter (A-Z)!";
-                form.password.focus();
-                return false;
-            }else {
-                document.getElementById("pwd").innerHTML = "";
-            }
-
-        } else if (form.password.value !== form.confirm_password.value){
-            document.getElementById("confirm").innerHTML = "Please check that you've entered and confirmed your password!";
-            form.confirm_password.focus();
-            return false;
-        }
-
-        return true;
+function checkForm(form) {
+    if (form.name.value === "") {
+        document.getElementById("nameField").innerHTML = "Username cannot be blank";
+        form.name.focus();
+        return false;
     }
 
+    var truth = validateEmail(form.username.value);
 
-function signUp() {
-    var name = document.getElementById('name_field').value;
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    var confirm_password = document.getElementById('confirm_password').value;
-    var terms = document.getElementById('terms_policy').checked;
+    if (truth !== true) {
+        document.getElementById("user").innerHTML = "Email Format is not correct";
+        form.username.focus();
+        return false;
+    } else {
+        document.getElementById("user").innerHTML = "";
 
-    function validateEmail(email) {
-        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(email);
     }
 
+    if (document.getElementById('terms_policy').checked !== true) {
+        document.getElementById("policy").innerHTML = "Please Read the policy and tick ✔";
+        return false;
+    } else {
+        document.getElementById("policy").innerHTML = "";
 
-    function checkPassword(pw1, pw2) {
-        if (pw1 !== pw2){
-            return false;
-        }else if (pw1 === ""){
-            return false;
-        }else if (pw2 === ""){
-            return false;
-        }else if (pw1 === pw2) {
-            return true;
-        }
     }
 
-    var check = checkPassword(password,confirm_password);
+    if (form.password.value === form.confirm_password.value) {
+        document.getElementById("confirm").innerHTML = "";
+    }
 
-    if (validateEmail(username) && check === true && name !== "" && terms === true){
-        document.getElementById("signForm").submit();// Form submission
-    }else {
-        if (validateEmail(username) === false){
-            document.getElementById("user").innerHTML = "Email Format is not correct";
-        }else {
-            document.getElementById("user").innerHTML = "";
-        }
-
-        if (username === ""){
-            document.getElementById("user").innerHTML = "Email Required";
-        }else {
-            document.getElementById("user").innerHTML = "";
-        }
-
-        if (check !== true){
-            document.getElementById("confirm").innerHTML = "Passwords Do Not Match";
-        }else {
-            document.getElementById("confirm").innerHTML = "";
-        }
-
-        if (password === ""){
-            document.getElementById("pwd").innerHTML = "Password is Required";
-        }else {
+    if (form.password.value !== "" && form.password.value === form.confirm_password.value) {
+        if (form.password.value.length < 8) {
+            document.getElementById("pwd").innerHTML = "Password must contain at least eight (8) characters";
+            form.password.focus();
+            return false;
+        } else {
             document.getElementById("pwd").innerHTML = "";
         }
 
-        if (name === ""){
-            document.getElementById("nameField").innerHTML = "Name is Required";
-        }else {
-            document.getElementById("nameField").innerHTML = "";
+        if (form.password.value === form.username.value) {
+            document.getElementById("pwd").innerHTML = "Password must be different from Email";
+            form.password.focus();
+            return false;
+        } else {
+            document.getElementById("pwd").innerHTML = "";
         }
 
-        if (terms !== true){
-            document.getElementById("policy").innerHTML = "Please Read the policy and tick ✔";
-        }else {
-            document.getElementById("policy").innerHTML = "";
+        re = /[0-9]/;
+        if (!re.test(form.password.value)) {
+            document.getElementById("pwd").innerHTML = "Password must contain at least one number (0-9)";
+            form.password.focus();
+            return false;
+        } else {
+            document.getElementById("pwd").innerHTML = "";
         }
+
+        re = /[a-z]/;
+        if (!re.test(form.password.value)) {
+            document.getElementById("pwd").innerHTML = "Password must contain at least one lowercase letter (a-z)!";
+            form.password.focus();
+            return false;
+        } else {
+            document.getElementById("pwd").innerHTML = "";
+        }
+
+        re = /[A-Z]/;
+        if (!re.test(form.password.value)) {
+            document.getElementById("pwd").innerHTML = "Password must contain at least one uppercase letter (A-Z)!";
+            form.password.focus();
+            return false;
+        } else {
+            document.getElementById("pwd").innerHTML = "";
+        }
+
+    } else if (form.password.value !== form.confirm_password.value) {
+        document.getElementById("confirm").innerHTML = "Please check that you've entered and confirmed your password!";
+        form.confirm_password.focus();
+        return false;
     }
 
+    return true;
 }
 
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+function checkSignUpEmail(email) {
+    var result = validateEmail(email);
+    if (result !== true){
+        document.getElementById("password").innerHTML = "Please check that you've entered and confirmed your email!";
+        return false;
+    }else {
+        return true;
+    }
+}
 
 function dropbox() {
     options = {
