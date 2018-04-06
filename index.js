cce094e53943a2d61b8943a9de3e7fb3ff47708b
franciskornu-09 <franciskornu@gmail.com>
2018-04-06 09:37:51 +0000

    /update_advert

--- index.js
+++ index.js
@@ -593,7 +593,7 @@ app.post('/update_advert/:id', upload.array('adverts'), function (req, res) {
 
         }).then(function (sticker) {
 
-            console.log("DELETING image" + JSON.stringify(sticker));
+            console.log("DELETING image" + JSON.stringify(stickerDetails));
             _.each(fileDetails, function (file) {
                 //Delete tmp fil after upload
                 var tempFile = file.path;
