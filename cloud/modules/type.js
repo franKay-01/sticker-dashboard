4a58383e64bd9e42d43801baeded8e166262aff3
pixelbendr <dev@psyphertxt.com>
2018-04-12 15:54:29 +0000

    trying to fix type object error

--- cloud/modules/type.js
+++ cloud/modules/type.js
@@ -1,25 +1,9 @@
 //TODO investgate how to auto add numbers
 type = {
     MESSAGES: {
-        visitor: 0,
-        creator: 1,
-        brand: 2
-    },
-    PACK_STATUS: {
-        pending: 0,
-        review: 1,
-        approved: 2
-    },
-    USER: {
-        super: 0,
-        normal: 2
-    },
-    CATALOGUE: {
-        text: 0,
-        image: 1,
-        quote: 2,
-        sticker: 3,
-        divider: 4
+        "visitor": 0,
+        "creator": 1,
+        "brand": 2
     }
 
 };
