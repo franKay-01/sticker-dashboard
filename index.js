de54cd68d45b0b7f70d600d9a091a8aa58af74b9
pixelbendr <dev@psyphertxt.com>
2018-04-25 14:42:05 +0000

    added header to resolve redirects

--- index.js
+++ index.js
@@ -211,12 +211,22 @@ app.all('*', function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
+    var schema = req.headers["x-forwarded-proto"];
+
+    if (schema === "https") {
+        req.connection.encrypted = true;
+    }
     next();
 });
 
 app.all('/', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    var schema = req.headers["x-forwarded-proto"];
+
+    if (schema === "https") {
+        req.connection.encrypted = true;
+    }
     next();
 });
 
