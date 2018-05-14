7b404869f5457561cf2cd159a134396ed56823cc
Francis Kornu <franciskornu@gmail.com>
2018-05-14 15:25:05 +0000

    working on advert page

--- index.js
+++ index.js
@@ -720,7 +720,7 @@ app.post('/update_advert_image/:id', upload.array('adverts'), function (req, res
         }).then(function (advert_image) {
 
             console.log("ADVERT "+JSON.stringify(advert_image));
-            return new Parse.Query(AdvertImageClass).equalTo("objectId", advert_image.id).first();
+            return new Parse.Query(AdvertImageClass).equalTo("objectId", id).first();
 
         }).then(function (advert) {
 
