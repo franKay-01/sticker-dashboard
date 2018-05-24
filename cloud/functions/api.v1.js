9291516b23c8bb4d3db43b462297efcd260609a9
pixelbendr <dev@psyphertxt.com>
2018-05-24 12:13:12 +0000

    added query to retrieve packs and stickers

--- cloud/functions/api.v1.js
+++ cloud/functions/api.v1.js
@@ -4,16 +4,18 @@ let _ = require('underscore');
 
 let PacksClass = "Packs";
 
-Parse.Cloud.define("getStickers", function (req, res) {
+Parse.Cloud.define("getPacks", function (req, res) {
 
-    res.success(util.setResponseOk(process.env.ADMIN));
+    return new Parse.Query(PacksClass).equalTo("user_id", process.env.ADMIN).find({useMasterKey: true})
+        .then(function (packs) {
 
-   /* // var user = req.user;
-    return new Parse.Query(PacksClass).equalTo("objectId", "EksXNOeVKj").first({useMasterKey: true})
-        .then(function (pack) {
+            let promises = _.map(packs, function (pack) {
+                return pack.relation(PacksClass).query().find({useMasterKey: true});
+            });
 
-            let stickers = pack.relation(PacksClass);
-            return stickers.query().find({useMasterKey: true});
+            return Parse.Promise.when(promises);
+
+            //  return stickers.query().find({useMasterKey: true});
 
         }).then(function (stickers) {
 
@@ -22,23 +24,6 @@ Parse.Cloud.define("getStickers", function (req, res) {
         }, function (error) {
 
             util.handleError(res, error);
-        });*/
-
-});
-
-Parse.Cloud.define("getSticker", function (req, res) {
-
-    let stickerId = req.params.stickerId;
-    // var user = req.user;
-
-    stickers.findById(stickerId).then(function (badge) {
-
-        res.success(util.setResponseOk(badge));
-
-    }, function (error) {
-
-        util.handleError(res, error);
-
-    });
+        });
 
 });
