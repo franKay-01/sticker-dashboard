089d327d18db4ce5a6b6ecac1e1677f8690e9082
pixelbendr <dev@psyphertxt.com>
2018-05-24 11:40:11 +0000

    testing env in cloud code

--- cloud/functions/getStickers.js
+++ cloud/functions/getStickers.js
@@ -6,42 +6,6 @@ var stickers = require("../modules/stickers");
 
 let PacksClass = "Packs";
 
-Parse.Cloud.define("getStickers", function (req, res) {
-
-    // var user = req.user;
-    return new Parse.Query(PacksClass).equalTo("objectId", "EksXNOeVKj").first({useMasterKey: true})
-        .then(function (pack) {
-
-            let stickers = pack.relation(PacksClass);
-            return stickers.query().find({useMasterKey: true});
-
-        }).then(function (stickers) {
-
-            res.success(util.setResponseOk(stickers));
-
-        }, function (error) {
-
-            util.handleError(res, error);
-        });
-
-});
-
-Parse.Cloud.define("getSticker", function (req, res) {
-
-    let stickerId = req.params.stickerId;
-    // var user = req.user;
-
-    stickers.findById(stickerId).then(function (badge) {
-
-        res.success(util.setResponseOk(badge));
-
-    }, function (error) {
-
-        util.handleError(res, error);
-
-    });
-
-});
 
 Parse.Cloud.define("getStory", function (req, res) {
 
