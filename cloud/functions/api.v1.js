1b3675a4dbea0a91b64ba95b57a00f4e9482f632
pixelbendr <dev@psyphertxt.com>
2018-05-29 15:08:45 +0000

    testing packs with sticker list

--- cloud/functions/api.v1.js
+++ cloud/functions/api.v1.js
@@ -40,52 +40,52 @@ Parse.Cloud.define("getPacks", function (req, res) {
 
             //todo check if pack is published
             //todo check if pack has not been archived
-            // _.map(_packs, pack => {
-            //
-            //     let packItem = {};
-            //     packItem.name = pack.get("pack_name");
-            //     packItem.description = pack.get("pack_description");
-            //     packItem.artwork = pack.get("art_work").url();
-            //
-            //     console.log("packItem");
-            //     console.log(JSON.stringify(packItem));
-            //
-            //     _.map(stickerList[0], function (stickers) {
-            //
-            //         console.log("Sticker List");
-            //
-            //         if (stickers.length !== 0) {
-            //
-            //             console.log("Sticker Length");
-            //             //todo choose five stickers for preview
-            //
-            //             let _stickers = [];
-            //             _.map(stickers, sticker => {
-            //
-            //                 console.log("Stickers");
-            //                 console.log(JSON.stringify(stickers));
-            //
-            //                 if (pack.id === sticker.get("parent").id) {
-            //
-            //                     console.log("Matches Parent");
-            //
-            //                     _stickers.push(sticker)
-            //                 }
-            //
-            //             });
-            //
-            //             packItem.stickers = [];
-            //             packItem.stickers = _stickers;
-            //             stickerObjects.push(packItem);
-            //             console.log("stickers");
-            //             console.log(JSON.stringify(packItem));
-            //
-            //         }
-            //     });
-            // });
+            _.map(_packs, pack => {
+
+                let packItem = {};
+                packItem.name = pack.get("pack_name");
+                packItem.description = pack.get("pack_description");
+                packItem.artwork = pack.get("art_work").url();
+
+                console.log("packItem");
+                console.log(JSON.stringify(packItem));
+
+                _.map(stickerList, function (stickers) {
+
+                    console.log("Sticker List");
+
+                    if (stickers.length !== 0) {
+
+                        console.log("Sticker Length");
+                        //todo choose five stickers for preview
+
+                        let _stickers = [];
+                        _.map(stickers, sticker => {
+
+                            console.log("Stickers");
+                            console.log(JSON.stringify(stickers));
+
+                            if (pack.id === sticker.get("parent").id) {
+
+                                console.log("Matches Parent");
+
+                                _stickers.push(sticker)
+                            }
+
+                        });
+
+                        packItem.stickers = [];
+                        packItem.stickers = _stickers;
+                        stickerObjects.push(packItem);
+                        console.log("stickers");
+                        console.log(JSON.stringify(packItem));
+
+                    }
+                });
+            });
 
 
-            res.success(util.setResponseOk(stickerList));
+            res.success(util.setResponseOk(stickerObjects));
 
         }, function (error) {
 
