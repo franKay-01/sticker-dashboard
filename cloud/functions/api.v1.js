30c06d4bdea6ff323b147578a366820a718ae054
pixelbendr <dev@psyphertxt.com>
2018-06-01 15:06:43 +0000

    testing story items

--- cloud/functions/api.v1.js
+++ cloud/functions/api.v1.js
@@ -150,7 +150,7 @@ Parse.Cloud.define("getStory", function (req, res) {
                 let storyItem = [];
                 _.each(_storyItems, storyItem => {
                  //   storyItem.push({content: storyItem.get("content"), type: storyItem.get("type")})
-                    console.log("content " + storyItem.get("content"))
+                    console.log("macbook " + storyItem.get("content"))
                 });
                 story.stories = storyItem;
             }
