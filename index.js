fde010004a3369a3427e4ce47209fb4894c1b27e
Francis Kornu <franciskornu@gmail.com>
2018-06-08 11:33:06 +0000

    working on resizing images

--- index.js
+++ index.js
@@ -2347,6 +2347,7 @@ app.post('/uploads', upload.array('im1[]'), function (req, res) {
                     sticker.set("sold", false);
                     // sticker.setACL(setPermission(_user, false));
 
+                    console.log("BITMAP " + JSON.stringify(bitmap));
                     sharp(bitmap)
                         .resize(100)
                         .toBuffer()
