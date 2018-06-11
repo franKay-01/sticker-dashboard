8621160773a128c7f8624ed3727c407a0c2c5832
Francis Kornu <franciskornu@gmail.com>
2018-06-11 12:03:15 +0000

    trying out jimp module

--- index.js
+++ index.js
@@ -2440,9 +2440,9 @@ app.post('/uploads', upload.array('im1[]'), function (req, res) {
                             if (e) throw e
                             console.log("BASE 64 : " + img64);
 
-                            let parseFile = new Parse.File(sticker_name[counter], img64, mime[counter]);
+                            let parseFile = new Parse.File(sticker_name[counter], {base64: img64} , mime[counter]);
 
-                            let newPreview = new Parse.Query(StickerClass).eqaulTo("objectId", sticker_id).first();
+                            let newPreview = new Parse.Query(StickerClass).equalTo("objectId", sticker_id).first();
                             let preview = new newPreview();
 
                             preview.set("preview", parseFile);
