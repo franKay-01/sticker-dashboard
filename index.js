c0d981514ceae5d3cc826a6d38963bba6196d89f
pixelbendr <dev@psyphertxt.com>
2018-06-15 09:49:58 +0000

    added a routes folder

--- index.js
+++ index.js
@@ -184,7 +184,7 @@ app.use(bodyParser.urlencoded({
 
 app.use(methodOverride());
 
-app.use(require("routes/newsletter"));
+app.use(require("./routes/newsletter"));
 
 app.use('/', (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
