bb8b2e6e91408b6803bebf9c4938522818fe792d
pixelbendr <dev@psyphertxt.com>
2018-06-15 09:59:30 +0000

    trying to seperate routes

--- routes/newsletter.js
+++ routes/newsletter.js
@@ -1,9 +1,9 @@
 let express = require('express');
 let router = express.Router();
 
-router.use(function timeLog(req, res, next) {
-    next();
-});
+// router.use(function timeLog(req, res, next) {
+//     next();
+// });
 
 router.get('/newsletter/:id', function (req, res) {
 
