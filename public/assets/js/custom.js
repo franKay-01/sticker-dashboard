4f80dfcee62123c396078bd8b6e208f48c04ef50
Francis Kornu <franciskornu@gmail.com>
2018-07-26 18:12:47 +0000

    working on curated packs

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -23,6 +23,24 @@ $(document).ready(function () {
 
         });
 
+        var selectedItems = [];
+        $('#easySelectable').easySelectable({
+            onSelected: function (el) {
+                selectedItems.push(el.attr("data-id"))
+
+                console.log("onSelected")
+                console.log(selectedItems)
+            },
+            onUnSelected: function (el) {
+                selectedItems = selectedItems.filter(function( obj ) {
+                    return obj !== el.attr("data-id");
+                });
+
+                console.log("onUnSelected")
+                console.log(selectedItems)
+            }
+        });
+
         //show collection form
         //TODO convert id's to classes
 
