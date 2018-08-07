aef03965d26d10f0e24767f6406607c8bdabdd0c
Francis Kornu <franciskornu@gmail.com>
2018-08-07 15:24:29 +0000

    working on packs - setting and updating productIds

--- index.js
+++ index.js
@@ -3907,6 +3907,7 @@ app.post('/pack/product/update', function (req, res) {
     let _stickers = [];
     let zero = "0";
 
+    console.log("PACK ID " + packId + " ProDUCT " + productId);
     if (token) {
 
         getUser(token).then(function (sessionToken) {
