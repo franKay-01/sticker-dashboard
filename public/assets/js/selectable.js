aecb75229b5628f54325d3010c47a9a10e8de20c
Francis Kornu <franciskornu@gmail.com>
2018-08-09 16:32:13 +0000

    adding version to pack edit

--- public/assets/js/selectable.js
+++ public/assets/js/selectable.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
                 });
                 $("#stickerIds").val(selectedItems);
                 $('.add-sticker-btn').removeAttr('disabled');
-                $('.add-sticker-btn').attr("background-color", "#dfdfdf");
+                $('.add-sticker-btn').attr("background-color", "#a46580");
 
             },
             onUnSelected: function (el) {
@@ -25,7 +25,6 @@ $(document).ready(function () {
 
                 if (selectedItems.length === 0) {
                     $('.add-sticker-btn').attr("disabled", true);
-                    $('.add-sticker-btn').attr("background-color", "#a46580");
                 }
             }
         });
