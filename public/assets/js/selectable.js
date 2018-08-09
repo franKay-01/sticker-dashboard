eed5483ae9e1559dc37e750b7d0db04f832fc53b
Francis Kornu <franciskornu@gmail.com>
2018-08-09 16:47:35 +0000

    adding version to pack edit

--- public/assets/js/selectable.js
+++ public/assets/js/selectable.js
@@ -2,7 +2,8 @@ $(document).ready(function () {
 
     $(function () {
 
-        var selectedItems = [];
+        let selectedItems = [];
+        let counter = 0;
         $('#easySelectable').easySelectable({
             onSelected: function (el) {
                 selectedItems.push(el.attr("data-id"))
@@ -11,10 +12,10 @@ $(document).ready(function () {
                     "border-width": "2px",
                     "border-style": "solid"
                 });
+                counter = counter + 1;
                 $("#stickerIds").val(selectedItems);
                 $('.add-sticker-btn').removeAttr('disabled');
-                $('.add-sticker-btn').attr("background-color", "#a46580");
-
+                $('.add-sticker-btn').html('ADD' + counter + 'STICKER(S)');
             },
             onUnSelected: function (el) {
                 selectedItems = selectedItems.filter(function (obj) {
@@ -22,9 +23,13 @@ $(document).ready(function () {
                 });
                 el.css({"border": "none"});
                 $("#stickerIds").val(selectedItems);
+                counter = counter - 1;
+                $('.add-sticker-btn').html('ADD' + counter + 'STICKER(S)');
+
 
                 if (selectedItems.length === 0) {
                     $('.add-sticker-btn').attr("disabled", true);
+                    $('.add-sticker-btn').html('ADD STICKER(S)');
                 }
             }
         });
