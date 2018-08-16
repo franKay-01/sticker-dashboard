e10535e2302aef16442fd2c0758bf8a6d2d0471d
Francis Kornu <franciskornu@gmail.com>
2018-08-16 12:07:21 +0000

    testing the app

--- tester.js
+++ tester.js
@@ -4,7 +4,7 @@ Parse.serverURL = process.env.SERVER_URL;
 
 module.exports = {
     login: function (username, password) {
-        console.log("USER " + username);
+        console.log("USER FROM TEST " + username);
         Parse.Cloud.run("login", {
             username: username,
             password: password
