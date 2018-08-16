44c3f55a9f2451f1c272ccdf38b5a869fce71913
Francis Kornu <franciskornu@gmail.com>
2018-08-16 12:24:08 +0000

    testing the app

--- test/index-spec.js
+++ test/index-spec.js
@@ -4,9 +4,11 @@ let loggingMachine = require('../tester');
 describe('loggingMachine', function() {
     describe('log In', function() {
         it('logs into account', function () {
-           loggingMachine.login("dev@psyphertxt.com", "WonDerful1").then(function (result) {
-               assert.equal(result, "dev@psyphertxt.com");
-           })
+            assert.equal(result, "dev@psyphertxt.com");
+
+           //  loggingMachine.login("dev@psyphertxt.com", "WonDerful1").then(function (result) {
+           //     assert.equal(result, "dev@psyphertxt.com");
+           // })
         });
     });
 });
