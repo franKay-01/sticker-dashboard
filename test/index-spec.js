bd3e4bf10896288a25c5682596e80d39ed1f1ae1
Francis Kornu <franciskornu@gmail.com>
2018-08-21 14:23:42 +0000

    working on new tests

--- test/index-spec.js
+++ test/index-spec.js
@@ -5,8 +5,25 @@ let Parse = require("parse/node").Parse; // import the module
 Parse.initialize(process.env.APP_ID);
 Parse.serverURL = process.env.SERVER_URL;
 
+function delayedMap(array, transform, callback) {
+    setTimeout(function() {
+        // callback(array.map(transform));
+    }, 100);
+}
+
 describe('loggingMachine', function () {
     describe('log In', function () {
+
+        it('eventually returns the results', function() {
+            var input = [1, 2, 3];
+            var transform = function(x) { return x * 2; };
+
+            delayedMap(input, transform, function(result) {
+                assert.deepEqual(result, [2, 4, 6]);
+                done();
+            });
+        });
+
         it('logs into account', function () {
 
             // new Parse.Query(_class.Authors).limit(1).find().then(function (data) {
@@ -23,6 +40,7 @@ describe('loggingMachine', function () {
                 Parse.User.become(user.getSessionToken()).then((user) => {
 
                     data = user.getSessionToken().getUsername();
+                    console.log("DATA INFO " + data);
                     assert.deepEqual(data, "dev@psyphertxt.co");
 
                     // assert.equal(data, "dev@psyphertxt.co");
