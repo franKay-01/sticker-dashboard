d065c14d3306dbc9b4f92591c3eb35c5b9fea107
Francis Kornu <franciskornu@gmail.com>
2018-08-21 14:17:13 +0000

    working on new tests

--- test/index-spec.js
+++ test/index-spec.js
@@ -1,6 +1,5 @@
 let assert = require('assert');
 let expect = require('chai').expect;
-let _class = require('../cloud/modules/classNames');
 
 let Parse = require("parse/node").Parse; // import the module
 Parse.initialize(process.env.APP_ID);
@@ -9,37 +8,35 @@ Parse.serverURL = process.env.SERVER_URL;
 describe('loggingMachine', function () {
     describe('log In', function () {
         it('logs into account', function () {
-            let dat = 0;
-            let data = "";
 
-            new Parse.Query(_class.Authors).limit(1).find().then(function (data) {
-
-                dat = data.length;
-                assert.equal(dat, 9);
-
-            });
-
-            // Parse.Cloud.run("login", {
-            //     username: "dev@psyphertxt.com",
-            //     password: "WonDerful1"
-            // }).then(user => {
-            //     Parse.User.become(user.getSessionToken()).then((user) => {
+            // new Parse.Query(_class.Authors).limit(1).find().then(function (data) {
             //
-            //         data = user.getSessionToken().getUsername();
-            //         assert.equal(data, "dev@psyphertxt.com", 'element is equal');
+            //     dat = data.length;
+            //     assert.equal(dat, 1);
             //
-            //         // assert.equal(data, "dev@psyphertxt.co");
-            //         done();
-            //         // expect(data).to.equal("dev@psyphertxt.co");
-            //
-            //     }, error => {
-            //
-            //         done(error);
-            //         // return;
-            //
-            //
-            //     });
             // });
+
+            Parse.Cloud.run("login", {
+                username: "dev@psyphertxt.com",
+                password: "WonDerful1"
+            }).then(user => {
+                Parse.User.become(user.getSessionToken()).then((user) => {
+
+                    data = user.getSessionToken().getUsername();
+                    assert.deepEqual(data, "dev@psyphertxt.com");
+
+                    // assert.equal(data, "dev@psyphertxt.co");
+                    done();
+                    // expect(data).to.equal("dev@psyphertxt.co");
+
+                }, error => {
+
+                    done(error);
+                    // return;
+
+
+                });
+            });
 
 
         });
