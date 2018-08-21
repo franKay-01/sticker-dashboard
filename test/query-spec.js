fd2652a9f476c0f66840ba58132b61534a669a85
Francis Kornu <franciskornu@gmail.com>
2018-08-21 12:19:07 +0000

    working on new tests

--- test/query-spec.js
+++ test/query-spec.js
@@ -10,6 +10,7 @@ describe('queringMachine', function () {
     describe('Query Packs', function () {
         it('Queries into Packs', function () {
 
+            let dat = 0;
             let name = "";
             let verison = 0;
             let description = "";
@@ -17,23 +18,26 @@ describe('queringMachine', function () {
             let userId = "";
 
             new Parse.Query(_class.Packs).equalTo("version", 1).find().then(function (data) {
-                name = data[0].get("name");
-                verison = data[0].get("version");
-                description = data[0].get("description");
-                archive = data[0].get("archived");
-                userId = data[0].get("userId");
+                // name = data[0].get("name");
+                // verison = data[0].get("version");
+                // description = data[0].get("description");
+                // archive = data[0].get("archived");
+                // userId = data[0].get("userId");
+                //
+                // assert.isString(name, 'is(not) string');
+                // assert.isNumber(verison, 'is(not) number');
+                // assert.isString(description, 'is(not) string');
+                // assert.isBoolean(archive, 'is(not) boolean');
+                // assert.isString(userId, 'is(not) string');
+                // let preview = data[0].get("artwork").url();
+                //
+                // let extension = /[^.]+$/.exec(preview);
+                // assert.equal(extension, "jpg" | "png");
+                //
+                // console.log("DATA");
 
-                assert.isString(name, 'is(not) string');
-                assert.isNumber(verison, 'is(not) number');
-                assert.isString(description, 'is(not) string');
-                assert.isBoolean(archive, 'is(not) boolean');
-                assert.isString(userId, 'is(not) string');
-                let preview = data[0].get("artwork").url();
-
-                let extension = /[^.]+$/.exec(preview);
-                assert.equal(extension, "jpg" | "png");
-
-                console.log("DATA");
+                dat = data.length;
+                    assert.equal(dat, 2);
 
             });
 
