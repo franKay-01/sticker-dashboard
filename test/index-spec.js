1d3695a677701fed75ed3b49591b39b51bc8d91e
Francis Kornu <franciskornu@gmail.com>
2018-08-21 14:58:28 +0000

    working on new tests

--- test/index-spec.js
+++ test/index-spec.js
@@ -1,5 +1,6 @@
 let assert = require('assert');
 let expect = require('chai').expect;
+let _class = require('../cloud/modules/classNames');
 
 let Parse = require("parse/node").Parse; // import the module
 Parse.initialize(process.env.APP_ID);
@@ -9,38 +10,39 @@ describe('loggingMachine', function () {
     describe('log In', function () {
         it('logs into account', function () {
 
-            // new Parse.Query(_class.Authors).limit(1).find().then(function (data) {
-            //
-            //     dat = data.length;
-            //     assert.equal(dat, 1);
-            //
-            // });
-            Parse.Cloud.run("login", {
-                username: "dev@psyphertxt.com",
-                password: "WonDerful1"
-            }).then(user => {
-                Parse.User.become(user.getSessionToken()).then((user) => {
+            new Parse.Query(_class.Authors).limit(1).find().then(function (data) {
+                console.log("DATA INFO " + JSON.stringify(data));
 
-                    console.log("USER " + JSON.stringify(user));
-                    data = user.getUsername();
-                    let check = "dev@psyphertxt.co";
-
-                    console.log("DATA INFO " + JSON.stringify(data));
-                    console.log("DATA INFO CHECK" + JSON.stringify(check));
-                    assert.deepStrictEqual(data, check);
-                    done();
-
-
-                }, error => {
-
-                    done(error);
-                    // return;
-
-
-                });
-
+                dat = data.length;
+                assert.equal(dat, 1);
 
             });
+            // Parse.Cloud.run("login", {
+            //     username: "dev@psyphertxt.com",
+            //     password: "WonDerful1"
+            // }).then(user => {
+            //     Parse.User.become(user.getSessionToken()).then((user) => {
+            //
+            //         console.log("USER " + JSON.stringify(user));
+            //         data = user.getUsername();
+            //         let check = "dev@psyphertxt.co";
+            //
+            //         console.log("DATA INFO " + JSON.stringify(data));
+            //         console.log("DATA INFO CHECK" + JSON.stringify(check));
+            //         assert.deepStrictEqual(data, check);
+            //         done();
+            //
+            //
+            //     }, error => {
+            //
+            //         done(error);
+            //         // return;
+            //
+            //
+            //     });
+            //
+            //
+            // });
 
 
         });
