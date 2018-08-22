6347a48e84ff17208f91b581de9fac0449c66465
Francis Kornu <franciskornu@gmail.com>
2018-08-22 10:22:55 +0000

    working on testing

--- test/index-spec.js
+++ test/index-spec.js
@@ -27,7 +27,45 @@ function findAuthor(email) {
 
 describe('loggingMachine', function () {
     describe('log In', function () {
-        secondName = findAuthor("michael@info.com");
+
+        findAuthor("michael@info.com").then(function (name) {
+            it('logs into account', function () {
+                // assert.equal(result.length, 3);
+
+                console.log("NAME FROM IT " + name);
+                assert.deepEqual(name, "Michael bay");
+                done();
+
+                // Parse.Cloud.run("login", {
+                //     username: "dev@psyphertxt.com",
+                //     password: "WonDerful1"
+                // }).then(user => {
+                //     Parse.User.become(user.getSessionToken()).then((user) => {
+                //
+                //         console.log("USER " + JSON.stringify(user));
+                //         data = user.getUsername();
+                //         let check = "dev@psyphertxt.co";
+                //
+                //         console.log("DATA INFO " + JSON.stringify(data));
+                //         console.log("DATA INFO CHECK" + JSON.stringify(check));
+                //         assert.deepStrictEqual(data, check);
+                //         done();
+                //
+                //
+                //     }, error => {
+                //
+                //         done(error);
+                //         // return;
+                //
+                //
+                //     });
+                //
+                //
+                // });
+
+
+            });
+        });
 
 
         it('should start empty', function () {
@@ -36,43 +74,7 @@ describe('loggingMachine', function () {
             assert.equal(arr.length, 1);
         });
 
-        it('logs into account', function () {
-            this.timeout(30000);
-            // assert.equal(result.length, 3);
 
-            console.log("NAME " + secondName);
-            assert.deepEqual(secondName, "Michael bay");
-            done();
-
-            // Parse.Cloud.run("login", {
-            //     username: "dev@psyphertxt.com",
-            //     password: "WonDerful1"
-            // }).then(user => {
-            //     Parse.User.become(user.getSessionToken()).then((user) => {
-            //
-            //         console.log("USER " + JSON.stringify(user));
-            //         data = user.getUsername();
-            //         let check = "dev@psyphertxt.co";
-            //
-            //         console.log("DATA INFO " + JSON.stringify(data));
-            //         console.log("DATA INFO CHECK" + JSON.stringify(check));
-            //         assert.deepStrictEqual(data, check);
-            //         done();
-            //
-            //
-            //     }, error => {
-            //
-            //         done(error);
-            //         // return;
-            //
-            //
-            //     });
-            //
-            //
-            // });
-
-
-        });
     });
 
 });
