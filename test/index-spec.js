38a7df5be5a4fe86b1fb097465e177c7eef91164
Francis Kornu <franciskornu@gmail.com>
2018-08-23 09:47:31 +0000

    working on testing

--- test/index-spec.js
+++ test/index-spec.js
@@ -1,5 +1,6 @@
 let assert = require('assert');
 let expect = require('chai').expect;
+const test = require('tape'); // assign the tape library to the variable "test"
 let _class = require('../cloud/modules/classNames');
 
 let Parse = require("parse/node").Parse; // import the module
@@ -7,35 +8,45 @@ Parse.initialize(process.env.APP_ID);
 Parse.serverURL = process.env.SERVER_URL;
 
 
-describe('loggingMachine', function () {
-    describe('log In', function () {
-        it('should be object', function (done) {
 
-            new Promise(async (resolve, reject) => {
+function sum (a, b) {
+    // your code to make the test pass goes here ...
+    return a * b;
+}
 
-                let email = "michael@info.com";
-
-                let query = new Parse.Query(_class.Authors);
-                query.equalTo("email", email);
-                query.first().then(function (author) {
-
-                    console.log("AUTHER NAME " + author.get("name"));
-                    name = author.get("name");
-                    // expect(author).to.be.an('array', 'nooo why fail??');
-                    // assert.typeOf(author, '');
-                    expect(name).to.be.an('object');
-                })
-            });
-            done();
-
-        });
-
-        it('should start empty', function () {
-            var arr = [];
-
-            assert.equal(arr.length, 1);
-        });
-
-    });
-
+test('sum should return the addition of two numbers', function (t) {
+    t.equal(3, sum(1, 2)); // make this test pass by completing the add function!
+    t.end();
 });
+// describe('loggingMachine', function () {
+//     describe('log In', function () {
+//         it('should be object', function (done) {
+//
+//             new Promise(async (resolve, reject) => {
+//
+//                 let email = "michael@info.com";
+//
+//                 let query = new Parse.Query(_class.Authors);
+//                 query.equalTo("email", email);
+//                 query.first().then(function (author) {
+//
+//                     console.log("AUTHER NAME " + author.get("name"));
+//                     name = author.get("name");
+//                     // expect(author).to.be.an('array', 'nooo why fail??');
+//                     // assert.typeOf(author, '');
+//                     expect(name).to.be.an('object');
+//                 })
+//             });
+//             done();
+//
+//         });
+//
+//         it('should start empty', function () {
+//             var arr = [];
+//
+//             assert.equal(arr.length, 1);
+//         });
+//
+//     });
+//
+// });
