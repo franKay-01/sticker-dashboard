687fb73825b53672ea70027490a140adbbaef537
Francis Kornu <franciskornu@gmail.com>
2018-09-03 12:19:11 +0000

    writing more tests

--- test/upload-spec.js
+++ test/upload-spec.js
@@ -2,7 +2,7 @@ let fs = require('fs');
 const path = require('path');
 const test = require('tape'); // assign the tape library to the variable "test"
 let _class = require('../cloud/modules/classNames');
-const directoryPath = path.join(__dirname, '/public/assets/images');
+const directoryPath = path.join('/public/assets/images');
 
 
 let Parse = require("parse/node").Parse; // import the module
@@ -10,7 +10,7 @@ Parse.initialize(process.env.APP_ID);
 Parse.serverURL = process.env.SERVER_URL;
 
 //passsing directoryPath and callback function
-test('check typeOf of Pack items', function (t) {
+test('uploading items', function (t) {
     t.plan(1);
 
     fs.readdir(directoryPath, function (err, files) {
