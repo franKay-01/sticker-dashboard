8eb6a9e26e43504417c952016f721134e4c3f758
Francis Kornu <franciskornu@gmail.com>
2018-09-03 11:24:59 +0000

    writing more tests

--- test/login-spec.js
+++ test/login-spec.js
@@ -1,19 +1,19 @@
-// const test = require('tape'); // assign the tape library to the variable "test"
-//
-// let Parse = require("parse/node").Parse; // import the module
-// Parse.initialize(process.env.APP_ID);
-// Parse.serverURL = process.env.SERVER_URL;
-//
-// test('log into the system', function (t) {
-//
-//     Parse.Cloud.run("login", {
-//         username: 'dev@psyphertxt.com',
-//         password: 'Wonderful1'
-//     }).then(user => {
-//
-//         console.log("USER " + JSON.stringify(user));
-//         t.equal("true", true);
-//         t.end();
-//     })
-//
-// });
+const test = require('tape'); // assign the tape library to the variable "test"
+
+let Parse = require("parse/node").Parse; // import the module
+Parse.initialize(process.env.APP_ID);
+Parse.serverURL = process.env.SERVER_URL;
+
+test('log into the system', function (t) {
+
+    t.plan(1);
+
+    Parse.User.logIn(username, password).then(function (user) {
+        console.log("USER " + user);
+        let type = user.get("type");
+        t.equal(typeof type, "number");
+        t.end();
+
+    })
+
+});
