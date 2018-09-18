649be9666754a321790cbcf64b9f1ccb0b80d976
Francis Kornu <franciskornu@gmail.com>
2018-09-18 11:57:11 +0000

    working on advert routes

--- routes/adverts.js
+++ routes/adverts.js
@@ -1,6 +1,8 @@
 let _class = require('../cloud/modules/classNames');
 let util = require('../cloud/modules/util');
 let _ = require('underscore');
+let multer = require('multer');
+
 let storage = multer.diskStorage({
     destination: function (req, file, cb) {
         console.log("Dest " + JSON.stringify(file));
