1c21810d11438c42077346e5d3fca16753b981ad
Francis Kornu <franciskornu@gmail.com>
2018-10-01 10:06:55 +0000

    working on packs routes

--- routes/packs.js
+++ routes/packs.js
@@ -38,4 +38,64 @@ module.exports = function (app) {
         }
     });
 
+    app.post('/pack', function (req, res) {
+
+        let token = req.cookies.token;
+        let pack_description = req.body.pack_description;
+        let coll_name = req.body.coll_name;
+        let packType = parseInt(req.body.packType);
+        let version = parseInt(req.body.version);
+
+        if (token) {
+
+            let _user = {};
+
+            util.getUser(token).then(function (sessionToken) {
+
+                _user = sessionToken.get("user");
+
+                let PackCollection = new Parse.Object.extend(_class.Packs);
+                let pack = new PackCollection();
+                pack.set("name", coll_name);
+                pack.set("description", pack_description);
+                pack.set("userId", _user.id);
+                pack.set("status", type.PACK_STATUS.pending);
+                pack.set("version", version);
+                pack.set("productId", "");
+                pack.set("archived", false);
+                pack.set("flagged", false);
+                pack.set("published", false);
+                pack.set("previews", []);
+
+                if (packType === type.PACK_TYPE.grouped) {
+
+                    pack.set("packType", type.PACK_TYPE.grouped);
+
+                } else if (packType === type.PACK_TYPE.themed) {
+
+                    pack.set("packType", type.PACK_TYPE.themed);
+
+                } else if (packType === type.PACK_TYPE.curated) {
+
+                    pack.set("packType", type.PACK_TYPE.curated);
+
+                }
+
+                return pack.save();
+
+            }).then(function (collection) {
+
+                res.redirect('/pack/' + collection.id);
+
+            }, function (error) {
+                console.log("ERROR OCCURRED WHEN ADDING NEW PACK " + error.message);
+                console.log('/');
+            })
+        }
+        else {
+            res.redirect("/");
+        }
+    });
+
+
 };
