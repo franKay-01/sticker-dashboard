b11dfc1b49ae5ceeda8a7bd24e709fd0ce0d3837
Francis Kornu <franciskornu@gmail.com>
2018-10-25 10:11:26 +0000

    adding additional projects to existing packs

--- routes/projects.js
+++ routes/projects.js
@@ -201,7 +201,7 @@ module.exports = function (app) {
 
     });
 
-    app.get('/projectAdd/:itemType/:itemId/:projectId', function (req, res) {
+    app.get('/project/new/add/:itemType/:itemId/:projectId', function (req, res) {
 
         let token = req.cookies.token;
         let itemId = req.params.itemId;
