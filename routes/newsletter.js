21956c412984fdefe34147e1d8fcde0247fdff07
Francis Kornu <franciskornu@gmail.com>
2019-01-13 22:35:25 +0000

    working on previewing all story types

--- routes/newsletter.js
+++ routes/newsletter.js
@@ -96,7 +96,7 @@ module.exports = function (app) {
             });
 
         }, function (error) {
-            console.log("ERROR " + error.message);
+            console.log("ERROR NEWSLETTER " + error.message);
             res.redirect('/stories/' + projectId);
         })
 
