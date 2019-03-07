4d2f0bbd9e5db7b98e48b971ee705f7e9fe7b998
Francis Kornu <franciskornu@gmail.com>
2019-03-07 12:33:27 +0000

    working on product ids - apis

--- cloud/functions/api.dashboard.v1.js
+++ cloud/functions/api.dashboard.v1.js
@@ -23,9 +23,7 @@ Parse.Cloud.define("editPackDetails", function(req, res){
 
   console.log("PACK RE-EDIT " + packId + " " + version +" "+name+" "+description+" "+keywords+" "+archive);
 
-
-  return new Parse.Query(_class.Packs).equalTo("objectId", packId).first({useMasterKey: true})
-  .then(function(pack){
+  return new Parse.Query(_class.Packs).equalTo("objectId", packId).first({useMasterKey: true}).then(function(pack){
 
     console.log("PACK EDITED 1 " + JSON.stringify(pack));
 
