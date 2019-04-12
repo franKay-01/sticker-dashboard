1d9f1b5f9d5a0d7821d08abe3bb0878cfae5202f
Francis Kornu <franciskornu@gmail.com>
2019-04-12 11:37:10 +0000

    working on editing html storyItems

--- cloud/functions/api.dashboard.v1.js
+++ cloud/functions/api.dashboard.v1.js
@@ -17,6 +17,7 @@ Parse.Cloud.define("updateHtmlItem", function(req, res){
 
   return new Parse.Query(_class.StoryItems).equalTo("objectId", storyItemId).first({useMasterKey:true})
   .then(function(storyItem){
+    console.log("STORYITEM ###### "+ JSON.stringify(storyItem));
     let contents = storyItem.get("contents");
 
     let _html = contents.html[itemIndex];
@@ -41,7 +42,7 @@ Parse.Cloud.define("updateHtmlItem", function(req, res){
     return storyItem.save();
   }).then(function(saved){
 
-    res.success(util.setResponseOk(storyItemDetails));
+    res.success(util.setResponseOk(saved));
 
   }, function(error){
 
