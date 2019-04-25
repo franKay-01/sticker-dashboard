13db15da6f5775b20c63345c52789a4d2931395b
Francis Kornu <franciskornu@gmail.com>
2019-04-25 15:51:37 +0000

    working on saving stickers

---
+++ upload.js
@@ -0,0 +1,17 @@
+const IncomingForm = require('formidable').IncomingForm;
+
+module.exports = function upload(req, res) {
+  var form = new IncomingForm()
+
+  form.on('file', (field, file) => {
+
+    console.log("FILE PATH AS EXPECTED " + JSON.stringify(file));
+
+  });
+
+  form.on('end', () => {
+    res.json()
+  });
+  
+  form.parse(req)
+};
