0cc35514b6cc94f8c92d5ea95dad75bd805c8e7f
Francis Kornu <franciskornu@gmail.com>
2019-05-02 16:55:15 +0000

    working on changing feed status

--- cloud/modules/createDashboard.js
+++ cloud/modules/createDashboard.js
@@ -161,9 +161,9 @@ exports.FeedStickers = (stickers) => {
   let _stickers = [];
 
   if (stickers.length){
-    _.each(stickers, stickers => {
+    _.each(stickers, sticker => {
 
-      _stickers.push({id: stickers.id, name: stickers.get("name"), image: sticker.get("uri").url()});
+      _stickers.push({id: sticker.id, name: sticker.get("name"), image: sticker.get("uri").url()});
 
     });
   }
